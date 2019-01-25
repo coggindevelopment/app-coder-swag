ad6dc6ec287953383874ed98b98e0c24aec56232
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-01-25 11:25:57 -0600

    lesson-04 commit - Creating the Data Models

---
+++ coder-swag/Model/Product.swift
@@ -0,0 +1,23 @@
+//
+//  Product.swift
+//  coder-swag
+//
+//  Created by Nicholas Coggin on 1/25/19.
+//  Copyright © 2019 Nicholas Coggin. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+    
+    private(set) public var title: String
+    private(set) public var price: String
+    private(set) public var imageName: String
+    
+    init(title: String, price: String, imageName: String) {
+        self.title = title
+        self.price = price
+        self.imageName = price
+    }
+    
+}
