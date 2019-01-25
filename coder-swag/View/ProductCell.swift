67f0ca2c2058c6a4ebce6a7ff8efce4f4a2c06f1
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-01-25 06:46:03 -0600

    lesson-03 commit - Creating a UICollectionView

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Nicholas Coggin on 1/25/19.
+//  Copyright © 2019 Nicholas Coggin. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
